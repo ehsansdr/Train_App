de39ce80778690ea0b012faf30d3c2cd59f1ad97
ehsan_sadr <sehsan251@gmail.com>
2025-02-23 09:44:42 +0330

    job explain

--- src/main/java/com/example/trainproject/Job/TimeScheduledJob.java
+++ src/main/java/com/example/trainproject/Job/TimeScheduledJob.java
@@ -13,10 +13,10 @@ import java.util.Date;
 
 @Slf4j
 @Component
-@ConditionalOnExpression("'${Trian.enableJob}'=='true'")
+@ConditionalOnExpression("'${Trian.enableJob}'=='true'") //  means that the job will only be created if the Trian.enableJob property in your application’s properties or YAML file is set to true.
 public class TimeScheduledJob extends BaseScheduler implements Job {
 
-    @Autowired
+    //@Autowired // if you have @Autowired, the name of the method will go red. have this red line do not stop the program
     public TimeScheduledJob() {
         super("TimeScheduled", 5);
     }
@@ -32,8 +32,15 @@ public class TimeScheduledJob extends BaseScheduler implements Job {
     @Qualifier("timeScheduledJobDetail")
     public JobDetail timeScheduledJobDetail() {
         // Generate a unique job identity by appending current timestamp to the job name
+
+        // JobDetail: This represents the job configuration in Quartz. It contains information about the job,
+        // such as the job class and identity.
         return JobBuilder.newJob(TimeScheduledJob.class)
+                //withIdentity(): This sets the unique identity of the job. In this case,
+                // it appends the current system time (System.currentTimeMillis()) to ensure that the job name is unique.
                 .withIdentity("TimeScheduledJob-" + System.currentTimeMillis()) // Unique identity using timestamp
+                //storeDurably(): This ensures that the job is stored even if it is not triggered immediately.
+                // This is useful when you need to store jobs that may not always be running but are ready to be triggered later.
                 .storeDurably()  // This ensures the job is stored even if it's not triggered
                 .build();
     }
@@ -41,16 +48,25 @@ public class TimeScheduledJob extends BaseScheduler implements Job {
     @Bean
     @Qualifier("timeScheduledJobTrigger")
     public Trigger timeScheduledJobTrigger(@Qualifier("timeScheduledJobDetail") JobDetail timeScheduledJobDetail) {
+
+//        Trigger: A trigger in Quartz is used to define when and how often a job will be executed.
+
         return TriggerBuilder.newTrigger()
-                .forJob(timeScheduledJobDetail)  // Link the trigger to the job
+                // forJob(): Links the trigger to a specific job (in this case, timeScheduledJobDetail).
+                .forJob(timeScheduledJobDetail)  // Link the trigger to the job with bean injection
+                // startAt(): Specifies the time when the trigger should first fire.
+                // Here, we’re setting it to start 10 seconds after the application starts.
                 .startAt(new Date(System.currentTimeMillis() + (10 * 1000)))  // Start 10 seconds after application starts
+                // withSchedule(): Defines the scheduling rule.
+                // SimpleScheduleBuilder.simpleSchedule() creates a simple trigger that repeats indefinitely every 10 seconds.
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                         .withIntervalInSeconds(10)  // Execute every 10 seconds
                         .repeatForever())  // Repeat indefinitely
+                // withIdentity(): Sets a unique identifier for the trigger,
+                // just like for the job, ensuring no conflicts.
                 .withIdentity("TimeScheduledTrigger-" + System.currentTimeMillis() + "-" + Math.random())  // Unique trigger identity
                 .build();
     }
-
 
     @Bean
     @Qualifier("timeScheduledJobTScheduler")
@@ -61,15 +77,15 @@ public class TimeScheduledJob extends BaseScheduler implements Job {
         Scheduler scheduler = factory.getScheduler();
 
         // Check if the job exists and delete it
+        // This code checks if the job already exists using scheduler.checkExists(jobKey).
+        // If the job exists, it deletes the old job using scheduler.
+        // deleteJob(jobKey) before scheduling the new one.
         JobKey jobKey = job.getKey();
-//        This code checks if the job already exists using scheduler.checkExists(jobKey).
-//        If the job exists, it deletes the old job using scheduler.
-//        deleteJob(jobKey) before scheduling the new one.
         if (scheduler.checkExists(jobKey)) {
             log.info("Job {} already exists, deleting the old job.", jobKey);
             scheduler.deleteJob(jobKey);
         }
-        // be care fule this code handle the existence of job in the project if the job alredy exist you will get the exception
+        // be care ful this code handle the existence of job in the project if the job already exist you will get the exception
 
         // Now schedule the job again
         scheduler.scheduleJob(job, trigger);
