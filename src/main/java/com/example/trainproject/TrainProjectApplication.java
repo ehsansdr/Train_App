046ff7f823cf3dad555542ae49d8d481b018f075
ehsan_sadr <sehsan251@gmail.com>
2025-04-12 11:49:47 +0330

    kafka completed

--- src/main/java/com/example/trainproject/TrainProjectApplication.java
+++ src/main/java/com/example/trainproject/TrainProjectApplication.java
@@ -1,17 +1,26 @@
 package com.example.trainproject;
 
 import com.example.trainproject.service.KafkaProducerService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class TrainProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TrainProjectApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(TrainProjectApplication.class, args);
 
-    }
+  }
+
+  @Bean
+  public CommandLineRunner init(KafkaProducerService kafkaProducerService) {
+    return args -> {
+         kafkaProducerService.sendMessage("can you see ne shayan");
+    };
+  }
 
 }
--- src/main/java/com/example/trainproject/service/KafkaProducerService.java
+++ src/main/java/com/example/trainproject/service/KafkaProducerService.java
@@ -1,11 +1,13 @@
 package com.example.trainproject.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 
+@Slf4j
 @Service
 public class KafkaProducerService {
 
@@ -15,7 +17,18 @@ public class KafkaProducerService {
   private KafkaTemplate<String, String> kafkaTemplate;
 
   public void sendMessage(String message) {
-    kafkaTemplate.send(kafkaTopic, message);
+    kafkaTemplate.send(kafkaTopic, message)
+        .whenComplete((result, ex) -> {
+          if (ex != null) {
+            log.error("❌ Failed to send message: " + ex.getMessage());
+          } else {
+            log.info("✅ Message sent successfully to topic: " + kafkaTopic +
+                ", partition: " + result.getRecordMetadata().partition() +
+                ", offset: " + result.getRecordMetadata().offset());
+          }
+        });
   }
+
+
 
 }
