67b78ca77aeda146d19abc8013056d622f82f4d2
ehsan_sadr <sehsan251@gmail.com>
2025-04-15 12:09:29 +0330

    bug in receiving

--- src/main/java/com/example/trainproject/controller/KafkaMessageController.java
+++ src/main/java/com/example/trainproject/controller/KafkaMessageController.java
@@ -1,6 +1,8 @@
-package com.example.trainProject.controller;
+package com.example.trainproject.controller;
 
+import com.example.trainproject.Model.KafkaProduceMessage;
 import com.example.trainproject.service.KafkaProducerService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.protocol.types.Field.Str;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/kafkaProducer")
 public class KafkaMessageController {
@@ -18,8 +21,9 @@ public class KafkaMessageController {
 
 
   @PostMapping("/produce-message")
-  public String sendMessage(@RequestBody String message) {
-    kafkaProducerService.sendMessage(message);
+  public String sendMessage(@RequestBody KafkaProduceMessage message) {
+    kafkaProducerService.sendMessageJson(message);
+    log.info("message send successfully {}", message);
     return "Message sent: " + message;
   }
 
---
+++ src/main/java/com/example/trainproject/service/KafkaConsumerService.java
@@ -0,0 +1,26 @@
+package com.example.trainproject.service;
+
+import com.example.trainproject.Model.KafkaProduceMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumerService {
+
+  @Value(value = "${my.kafka.topic}")
+  private static String kafkaTopic;
+
+  @Value(value = "${my.kafka.consumer-group-id}")
+  private static String groupId;
+
+
+  @KafkaListener(topics = "${my.kafka.topic}" , groupId =" ${my.kafka.consumer-group-id}" )
+  public void consumeMessage(KafkaProduceMessage message) {
+    System.out.println(message.toString() +  " =====> received perfectly");
+  }
+
+
+}
