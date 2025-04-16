299c8db2f40bd023867f1d498026abae41211d07
ehsan_sadr <sehsan251@gmail.com>
2025-04-16 16:06:29 +0330

    add consume and consumeRawMessage KafkaConsumerService

--- src/main/java/com/example/trainproject/base/Service/KafkaConsumerService.java
+++ src/main/java/com/example/trainproject/base/Service/KafkaConsumerService.java
@@ -1,5 +1,9 @@
 package com.example.trainproject.base.Service;
 import com.example.trainproject.base.Model.KafkaProduceMessage;
+import com.example.trainproject.base.Util.Wapper.DataTransferObject;
+import com.example.trainproject.base.Util.Wapper.TransferDeserializer;
+import com.example.trainproject.base.Util.Wapper.TransferWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,11 +19,40 @@ public class KafkaConsumerService {
   @Value(value = "${my.kafka.consumer-group-id}")
   private static String groupId;
 
+  private ObjectMapper objectMapper = new ObjectMapper();
 
-  @KafkaListener(topics = "${my.kafka.topic}" , groupId =" ${my.kafka.consumer-group-id}" )
+  private TransferDeserializer transferDeserializer = new TransferDeserializer(objectMapper);
+
+
+  @KafkaListener(topics = "${my.kafka.topic}" , groupId = "${my.kafka.consumer-group-id}" )
   public void consumeMessage(KafkaProduceMessage message) {
     System.out.println(message.toString() +  " =====> received perfectly");
   }
+
+  @KafkaListener(topics = "${my.kafka.topic}")
+  public void consume(String messageJson) throws Exception {
+    TransferWrapper<?> wrapper = objectMapper.readValue(messageJson, TransferWrapper.class);
+    System.out.println("Received transfer from: " + wrapper.getSourceProject());
+
+    // Deserialize data
+    Class<?> classType = Class.forName(wrapper.getDataType());
+    DataTransferObject data = (DataTransferObject) objectMapper.convertValue(wrapper.getData(), classType);
+    data.validate();
+
+  }
+
+  @KafkaListener(topics = "${my.kafka.topic}", groupId = "${my.kafka.consumer-group-id}")
+  public void consumeRawMessage(String message) {
+    try {
+      TransferWrapper<?> wrapper = transferDeserializer.deserialize(message);
+      System.out.println("Received: " + wrapper.getData());
+      wrapper.validate();
+      // Route or use the data object!
+    } catch (Exception ex) {
+      log.error("Invalid transfer message", ex);
+    }
+  }
+
 
 
 }
