0d91b4f5b24232a44542e968f4f72451de4e7090
ehsan_sadr <sehsan251@gmail.com>
2025-04-19 13:52:06 +0330

    test of serialize and deserialize pass

--- src/main/java/com/example/trainproject/base/Util/Wapper/DataTransferObject.java
+++ src/main/java/com/example/trainproject/base/Util/Wapper/DataTransferObject.java
@@ -1,8 +1,7 @@
 package com.example.trainproject.base.Util.Wapper;
 
 public interface DataTransferObject {
-  // might add common methods here later if needed, loke start and end
   default void validate() {
-    // Optional override in each DTO
+
   }
 }
