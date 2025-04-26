7a276f1c631cf90149a17b4ce39f0c0b69c5abe9
ehsan_sadr <sehsan251@gmail.com>
2025-04-26 11:11:33 +0330

    add pageable to the /transaction/{{pageSize}}/{{pageNumber}}

--- src/main/java/com/example/trainproject/base/Controller/TransactionController.java
+++ src/main/java/com/example/trainproject/base/Controller/TransactionController.java
@@ -1,8 +1,10 @@
 package com.example.trainproject.base.Controller;
 
+import com.example.trainproject.base.Dto.GenericPaginatedResponse;
 import com.example.trainproject.base.Model.Transaction;
 import com.example.trainproject.base.Service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,16 +16,24 @@ public class TransactionController extends BaseController {
 
     private final TransactionService transactionService;
 
-  @GetMapping("/get-all-transaction")
-  public List<Transaction> getAllTransaction() {
-
-    return transactionService.getAlltransaction();
+  @GetMapping("/{pageSize}/{pageNumber}")
+  public GenericPaginatedResponse<List<Transaction>> getAllTransaction(
+      @PathVariable int pageSize,
+      @PathVariable int pageNumber,
+      @RequestParam(required = false) String sort
+  ) {
+    Page<Transaction> transactions =  transactionService.getAllTransaction(pageSize,pageNumber,sort);
+    List<Transaction> transactionList = transactions.getContent();
+    return GenericPaginatedResponse.success(transactionList,
+        pageNumber,
+        pageSize,
+        transactions.getTotalElements());
   }
 
   @PostMapping("/save-transaction")
   public Transaction saveTransaction(@RequestBody Transaction transaction) {
 
-    return transactionService.findtTransaction(transaction);
+    return transactionService.saveTransaction(transaction);
   }
 
 
--- src/main/java/com/example/trainproject/base/Service/TransactionService.java
+++ src/main/java/com/example/trainproject/base/Service/TransactionService.java
@@ -3,6 +3,10 @@ package com.example.trainproject.base.Service;
 import com.example.trainproject.base.Model.Transaction;
 import com.example.trainproject.base.Repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,11 +16,28 @@ import java.util.List;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
-    public List<Transaction> getAlltransaction(){
-        return transactionRepository.findAll();
+
+    public Page<Transaction> getAllTransaction(int pageSize,
+        int pageNumber,
+        String sort){
+        Pageable pageable;
+        // sorting part
+        if (sort != null) {
+            Sort sortOrder = Sort.by(sort.split(",")[0]).ascending();
+            if (sort.endsWith("desc")) {
+                sortOrder = sortOrder.descending();
+            }
+            pageable = PageRequest.of(pageNumber, pageSize, sortOrder);
+        } else {
+            Sort sortOrder = Sort.by("createdAt").descending();
+            pageable = PageRequest.of(pageNumber, pageSize, sortOrder);
+            pageable = PageRequest.of(pageNumber, pageSize, Sort.by("id").descending());
+        }
+        return transactionRepository.findAll(pageable);
     }
 
-    public Transaction findtTransaction(Transaction transaction){
+
+    public Transaction saveTransaction(Transaction transaction){
         return transactionRepository.save(transaction);
     }
 
