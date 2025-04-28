8bd1d85bafeb68c29db18f38947823444d9e28b2
ehsan_sadr <sehsan251@gmail.com>
2025-04-28 11:41:56 +0330

    add bean prototype

--- src/main/java/com/example/trainproject/base/Service/handler/NotificationProxy.java
+++ src/main/java/com/example/trainproject/base/Service/handler/NotificationProxy.java
@@ -1,10 +1,17 @@
 package com.example.trainproject.base.Service.handler;
 
 
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
+@Scope("prototype")
+@NoArgsConstructor
 public class NotificationProxy implements NotificationHandler {
 
   @Setter
