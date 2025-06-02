7808ab40947355e809f991c7c3113c46c2cd86a2
ehsan_sadr <sehsan251@gmail.com>
2025-06-02 10:53:50 +0330

    modify name model

--- src/main/java/com/example/trainproject/Model/User.java
+++ src/main/java/com/example/trainproject/Model/User.java
@@ -1,22 +1,20 @@
 package com.example.trainproject.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Entity
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Document(indexName = "user")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
+
+  @Id
+  private String id;
+
+  private String name;
+  private String surname;
+  private String email;
+
+
 }
