f81d70d23a0fa5afb11a8655226e2edc6b18f60f
ehsan_sadr <sehsan251@gmail.com>
2025-06-09 09:55:03 +0330

    update db connection log

--- src/main/java/com/example/trainproject/utils/ElasticsearchHealthService.java
+++ src/main/java/com/example/trainproject/utils/ElasticsearchHealthService.java
@@ -2,6 +2,7 @@ package com.example.trainproject.utils;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class ElasticsearchHealthService {
+
+  @Value("${spring.elasticsearch.uris}")
+  private String elasticsearchUris;
 
   private final ElasticsearchOperations elasticsearchOperations;
 
@@ -20,9 +24,9 @@ public class ElasticsearchHealthService {
   public void testConnection() {
     try {
       boolean health = elasticsearchOperations.indexOps(IndexCoordinates.of("test")).exists();
-      log.info("✅ Successfully connected to Elasticsearch");
+      log.info("Successfully connected to Uris : {}",  elasticsearchUris);
     } catch (Exception e) {
-      log.error("❌ Failed to connect to Elasticsearch: {}", e.getMessage());
+      log.error("Failed to connect to Elasticsearch: {}", e.getMessage());
     }
   }
 }
